44a73669d1a1df46b87097440e7212fee13f3dac
scottk2112 <shkupferman@gmail.com>
2016-01-10 11:41:15 -0500

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,5 +1,5 @@
 <?php
-	define("HOSTDB", "mysql.shk2112.com"); // The host you want to connect to.
+	define("HOSTDB", "mysql.shk2112.com"); // The host you want to connect to. lastest
 	define("USERDB", "scottadmin"); // The database username.
 	define("DATABASEDB", "Sleekarchive" // The database name
 	define("PASSWORDDB", "Ru5h2112"); // The database password. 
