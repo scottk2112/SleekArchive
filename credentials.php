5d50c5af35f9b87e4711c10f809f428097d3347a
scottk2112 <shkupferman@gmail.com>
2016-01-10 16:30:57 -0500

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,8 +1,8 @@
 <?php
-	define("HOSTDB", "mysql.shk2112.com"); // The host you want to connect to. lastest
+	define("HOSTDB", "shk2112_sleekarchive"); // The host you want to connect to. lastest
 	define("USERDB", "shkadmin"); // The database username.
 	define("DATABASEDB", "sleekarchive"); // The database name
-	define("PASSWORDDB", "Ru5h2112"); // The database password. 
+	define("PASSWORDDB", "spring123"); // The database password. 
 	define("JOURNAL_NAME", "My Journal"); // The Journal name.
 	define("APPPASSWORD", "isaac"); // The Journal password. 
 ?>
