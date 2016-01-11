78594307ca1a74ae4181f03b1ec1a598654d707a
scottk2112 <shkupferman@gmail.com>
2016-01-11 12:03:37 -0500

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -2,7 +2,7 @@
 	define("HOSTDB", "127.4.22.2:3306"); // The host you want to connect to. lastest
 	define("USERDB", "adminLuBYSAi"); // The database username.
 	define("DATABASEDB", "php"); // The database name
-	define("PASSWORDDB", "adminLuBYSAi"); // The database password. 
+	define("PASSWORDDB", "5qUZcclh1YB2"); // The database password. 
 	define("JOURNAL_NAME", "My Journal"); // The Journal name.
 	define("APPPASSWORD", "isaac"); // The Journal password. 
 ?>
